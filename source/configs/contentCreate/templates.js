34a6890fa4e47b0ab6c597c3092d56b778d1266b
EVGENY POBOYKIN <evgenypoboykin@gmail.com>
2020-09-23 13:17:24 +0700

    Delete import IContainer in styled-component tsx template

--- source/configs/contentCreate/templates.js
+++ source/configs/contentCreate/templates.js
@@ -13,7 +13,12 @@ const Interfaces = () => {
 
 const styleFile = (style, name) => {
     if (style === 'styled-components') {
-        return [`import styled from 'styled-components';`, 'import {IContainer} from "./interfaces";', '', 'export const Container = styled.div.attrs({ className: "' + name + '__container" })' + '``;'].join('\n');
+        return [
+            `import styled from 'styled-components';`,
+            'import {} from "./interfaces";',
+            '',
+            'export const Container = styled.div.attrs({ className: "' + name + '__container" })' + '``;',
+        ].join('\n');
     } else if (style === 'css') {
         return [`.${name}__container{`, '   ', '}'].join('\n');
     } else if (style === 'scss') {
@@ -35,20 +40,80 @@ const index = (name) => {
 };
 
 const componentJsWithoutLogic = (name, style) => {
-    return [`import React, { memo } from 'react';`, styleImport(style), '', `const ${name} = memo(() => {`, `    return ${styleTag(style, name)};`, `});`, `export default ${name};`].join('\n');
+    return [
+        `import React, { memo } from 'react';`,
+        styleImport(style),
+        '',
+        `const ${name} = memo(() => {`,
+        `    return ${styleTag(style, name)};`,
+        `});`,
+        `export default ${name};`,
+    ].join('\n');
 };
 const componentJsWithLogic = (name, style) => {
-    return [`import React, { memo } from 'react';`, `import ${name}_Logic from './logic';`, styleImport(style), '', `const ${name} = memo(() => {`, `    const [] = ${name}_Logic();`, `    return ${styleTag(style, name)};`, `});`, `export default ${name};`].join('\n');
+    return [
+        `import React, { memo } from 'react';`,
+        `import ${name}_Logic from './logic';`,
+        styleImport(style),
+        '',
+        `const ${name} = memo(() => {`,
+        `    const [] = ${name}_Logic();`,
+        `    return ${styleTag(style, name)};`,
+        `});`,
+        `export default ${name};`,
+    ].join('\n');
 };
 const componentTsWithoutLogic = (name, style) => {
-    return [`import React, { memo } from 'react';`, styleImport(style), 'import {} from "./interfaces";', '', `const ${name} : React.FC = memo(() => {`, `    return ${styleTag(style, name)};`, `});`, `export default ${name};`].join('\n');
+    return [
+        `import React, { memo } from 'react';`,
+        styleImport(style),
+        'import {} from "./interfaces";',
+        '',
+        `const ${name} : React.FC = memo(() => {`,
+        `    return ${styleTag(style, name)};`,
+        `});`,
+        `export default ${name};`,
+    ].join('\n');
 };
 const componentTsWithLogic = (name, style) => {
-    return [`import React, { memo } from 'react';`, `import ${name}_Logic from './logic';`, styleImport(style), 'import {} from "./interfaces";', '', `const ${name} : React.FC = memo(() => {`, `    const [] = ${name}_Logic();`, `    return ${styleTag(style, name)};`, `});`, `export default ${name};`].join('\n');
+    return [
+        `import React, { memo } from 'react';`,
+        `import ${name}_Logic from './logic';`,
+        styleImport(style),
+        'import {} from "./interfaces";',
+        '',
+        `const ${name} : React.FC = memo(() => {`,
+        `    const [] = ${name}_Logic();`,
+        `    return ${styleTag(style, name)};`,
+        `});`,
+        `export default ${name};`,
+    ].join('\n');
 };
 
 const logicCode = (name) => {
-    return [`import { useCallback } from 'react';`, '', `const ${name}_Logic = () => {`, '   ', '   // Logic ${name} START', '', '    // Logic ${name} END', '', '    return [];', '};', '', `export default ${name}_Logic;`].join('\n');
+    return [
+        `import { useCallback } from 'react';`,
+        '',
+        `const ${name}_Logic = () => {`,
+        '   ',
+        '   // Logic ${name} START',
+        '',
+        '    // Logic ${name} END',
+        '',
+        '    return [];',
+        '};',
+        '',
+        `export default ${name}_Logic;`,
+    ].join('\n');
 };
 
-module.exports = { index, componentJsWithoutLogic, componentJsWithLogic, componentTsWithoutLogic, componentTsWithLogic, styleFile, logicCode, Interfaces };
+module.exports = {
+    index,
+    componentJsWithoutLogic,
+    componentJsWithLogic,
+    componentTsWithoutLogic,
+    componentTsWithLogic,
+    styleFile,
+    logicCode,
+    Interfaces,
+};
